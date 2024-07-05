c59cd5f8605980b7cba140a393438cddc2d58bc1
Martin Thomson <mt@lowentropy.net>
2024-07-05 16:02:16 +1000

    Inline code sub/super script substitutions

--- fix-sub.py
+++ fix-sub.py
@@ -3,11 +3,12 @@
 import fileinput
 import re
 
-chars = "-+=()0123456789i"
-subtr = "₋₊₌₍₎₀₁₂₃₄₅₆₇₈₉ᵢ"
-suptr = "⁻⁺⁼⁽⁾⁰¹²³⁴⁵⁶⁷⁸⁹ⁱ"
+chars = "-+=()0123456789im"
+subtr = "₋₊₌₍₎₀₁₂₃₄₅₆₇₈₉ᵢₘ"
+suptr = "⁻⁺⁼⁽⁾⁰¹²³⁴⁵⁶⁷⁸⁹ⁱᵐ"
 
 pseudocode = re.compile(r"^(~~~~*) *pseudocode$")
+inlinecode = re.compile(r"(?:^|(?<=[^\\]))`")
 sub = re.compile(r"(?:<sub>([" + chars + r"]+)</sub>|(?<=\w)_([" + chars + r"]))")
 sup = re.compile(r"(?:<sup>([" + chars + r"]+)</sup>|(?<=\w)\^([" + chars + r"]))")
 
@@ -23,6 +24,22 @@ def tr(line, pattern, target):
     result += line[lastend:]
     return result
 
+def trcode(line, code):
+    result = ""
+    lastend = 0
+    for m in inlinecode.finditer(line):
+        span = line[lastend:m.start()]
+        if code:
+            span = tr(span, sub, subtr)
+            span = tr(span, sup, subtr)
+
+        code = not code
+        result += span + "`"
+        lastend = m.end()
+
+    result += line[lastend:]
+    return (result, code)
+
 end = None
 code = False
 for line in fileinput.input():
@@ -30,9 +47,10 @@ for line in fileinput.input():
         m = pseudocode.match(line)
         if m:
             end = m[1]
+        elif line == "":
+            code = False
         else:
-            # TODO Look for `code` instead
-            pass
+            (line, code) = trcode(line, code)
     else:
         if line.strip() != end:
             line = tr(line, sub, subtr)
